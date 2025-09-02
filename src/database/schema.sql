cd01d2313822adbb1f6c4526996840e4e854cab7
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-09-02 10:56:01 -0300

    Feat: create schema sql and conect bd

---
+++ src/database/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE notes_db;
+
+\c notes_db;
+
+
+
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    conteudo TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    mensagem TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
